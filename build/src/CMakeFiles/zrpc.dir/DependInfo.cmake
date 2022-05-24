57e11fb7fc62cbfd79dcf525f64755d0cdfa955a
zong <www.295102885@qq.com>
2022-05-24 18:53:57 +0800

    update

---
+++ build/src/CMakeFiles/zrpc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zong/projects/zrpc/src/zrpcapplication.cpp" "/home/zong/projects/zrpc/build/src/CMakeFiles/zrpc.dir/zrpcapplication.cpp.o"
+  "/home/zong/projects/zrpc/src/zrpcconfig.cpp" "/home/zong/projects/zrpc/build/src/CMakeFiles/zrpc.dir/zrpcconfig.cpp.o"
+  "/home/zong/projects/zrpc/src/zrpcprovider.cpp" "/home/zong/projects/zrpc/build/src/CMakeFiles/zrpc.dir/zrpcprovider.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "zrpc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../example"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/zrpc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../lib/libzrpc.pdb"
+  "../../lib/libzrpc.so"
+  "CMakeFiles/zrpc.dir/zrpcapplication.cpp.o"
+  "CMakeFiles/zrpc.dir/zrpcconfig.cpp.o"
+  "CMakeFiles/zrpc.dir/zrpcprovider.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zrpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
